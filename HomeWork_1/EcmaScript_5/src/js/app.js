9e8ea44ecce49d7028d2fdb82c74ad12608a864c
septoon <41443306+septoon@users.noreply.github.com>
2019-02-28 16:07:02 +0300

    change total

--- HomeWork_1/EcmaScript_5/src/js/app.js
+++ HomeWork_1/EcmaScript_5/src/js/app.js
@@ -1,12 +1,16 @@
-function raiting(arr) {
-    'use strict';
-
+function bestScore (arr) {
     var one = Math.max.apply(null, arr);
-    var sort  = arr.sort(function(a, b) {
+    var sort = arr.sort(function(a, b) {
         return b - a;
-    })
-    console.info( `Лучший балл ${one}
-    Средний балл топ 3: ${(sort[0] + sort[1] + sort[2]) / 3 ^ 0}`);
-}
+    });
+    
+    var top = arr.splice(0, 3);
+    var sum = top.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    });
+    var total = Math.floor(sum / top.length);
 
-raiting([74989, 74990, 74990, 62000, 58480, 61800]);
+    console.log(`Лучший балл: ${one} 
+    Средний балл топ 3: ${total}`); 
+};
+bestScore([74989, 74990, 74990, 62000, 58480, 61800]);
