3f2f8c6a13cb7b04063f4bc401cfc14b9afac68a
septoon <41443306+septoon@users.noreply.github.com>
2019-03-01 22:26:20 +0300

    task done

---
+++ HomeWork_3/esm/src/game.js
@@ -0,0 +1,17 @@
+import Character from './domain.js';
+
+export default class Game {
+  start() {
+    console.log('game started');
+  }
+}
+/* eslint-disable */
+export class GameSavingData {
+}
+
+export function readGameSaving() { // eslint-disable-line no-unused-vars
+}
+
+export function writeGameSaving() { // eslint-disable-line no-unused-vars
+}
+/* eslint-enable */
